db05f9aee906c7349c32d09eca80980bc506cd6d
Ahmed <bahlawan.maids@gmail.com>
2023-07-27 18:19:52 +0300

    Add Brand entity and BrandRepository and some unit tess for BrandRepository

--- ShoppingLibrary/src/main/java/com/shopping/library/entity/Category.java
+++ ShoppingLibrary/src/main/java/com/shopping/library/entity/Category.java
@@ -159,4 +159,9 @@ public class Category {
 
         return "/category-images/" + this.id + "/" + this.image;
     }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
---
+++ ShoppingWebParent/ShoppingBackend/src/main/java/com/shopping/admin/brand/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.shopping.admin.brand;
+
+import com.shopping.library.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+}
