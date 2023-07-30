828a9932bcbfe7d8300c1bbf1776bf0e54bffee0
Ahmed <bahlawan.maids@gmail.com>
2023-07-30 09:53:52 +0300

    Code Product Entity and Repository

---
+++ ShoppingWebParent/ShoppingBackend/src/main/java/com/shopping/admin/prouct/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.shopping.admin.prouct;
+
+import com.shopping.library.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
