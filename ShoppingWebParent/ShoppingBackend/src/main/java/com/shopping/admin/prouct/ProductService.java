81679d19d9a2e77ca05a455ad3ff4a7924b29d2e
Ahmed <bahlawan.maids@gmail.com>
2023-08-01 10:10:51 +0300

    Code save product

--- ShoppingWebParent/ShoppingBackend/src/main/java/com/shopping/admin/prouct/ProductService.java
+++ ShoppingWebParent/ShoppingBackend/src/main/java/com/shopping/admin/prouct/ProductService.java
@@ -3,16 +3,35 @@ package com.shopping.admin.prouct;
 import com.shopping.library.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.util.StringUtils;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class ProductService {
 
     @Autowired
-    private ProductRepository repo;
+    private ProductRepository productRepository;
 
     public List<Product> listAll() {
-        return (List<Product>) repo.findAll();
+        return productRepository.findAll();
+    }
+
+    public Product save(Product product) {
+        if (product.getId() == null) {
+            product.setCreatedTime(new Date());
+        }
+
+        if (StringUtils.isEmpty(product.getAlias())) {
+            String defaultAlias = product.getName().replaceAll(" ", "-");
+            product.setAlias(defaultAlias);
+        } else {
+            product.setAlias(product.getAlias().replaceAll(" ", "-"));
+        }
+
+        product.setUpdatedTime(new Date());
+
+        return productRepository.save(product);
     }
 }
