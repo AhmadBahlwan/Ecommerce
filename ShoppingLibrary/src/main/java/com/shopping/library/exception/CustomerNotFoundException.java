a0ef1e71f1c892717c22602535e887314609888d
Ahmed <bahlawan.maids@gmail.com>
2023-08-19 10:43:04 +0300

    Customer management module

---
+++ ShoppingLibrary/src/main/java/com/shopping/library/exception/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.shopping.library.exception;
+
+public class CustomerNotFoundException extends Exception {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+
+}
