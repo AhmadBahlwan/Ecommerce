b50e80b205eadbff2cdfa59420bfdf6baac29839
Ahmed <bahlawan.maids@gmail.com>
2023-08-26 09:54:31 +0300

    Code refactoring

--- ShoppingWebParent/ShoppingBackend/src/main/java/com/shopping/admin/product/ProductController.java
+++ ShoppingWebParent/ShoppingBackend/src/main/java/com/shopping/admin/product/ProductController.java
@@ -145,12 +145,20 @@ public class ProductController {
 
     @GetMapping("/products/edit/{id}")
     public String editProduct(@PathVariable("id") Integer id, Model model,
-                              RedirectAttributes ra) {
+                              RedirectAttributes ra, @AuthenticationPrincipal ShoppingUserDetails loggedUser) {
         try {
             Product product = productService.get(id);
             List<Brand> listBrands = brandService.listAll();
             Integer numberOfExistingExtraImages = product.getImages().size();
 
+            boolean isReadOnlyForSalesperson = false;
+
+            if (!loggedUser.hasRole("Admin") && !loggedUser.hasRole("Editor")) {
+                if (loggedUser.hasRole("Salesperson")) {
+                    isReadOnlyForSalesperson = true;
+                }
+            }
+            model.addAttribute("isReadOnlyForSalesperson", isReadOnlyForSalesperson);
             model.addAttribute("product", product);
             model.addAttribute("listBrands", listBrands);
             model.addAttribute("pageTitle", "Edit Product (ID: " + id + ")");
