f20be458971f0b3589a3c9505106ee1d01568b7e
Ahmed <bahlawan.maids@gmail.com>
2023-08-29 15:17:10 +0300

    Set default address

--- ShoppingWebParent/ShoppingFrontEnd/src/test/java/com/shopping/AddressRepositoryTests.java
+++ ShoppingWebParent/ShoppingFrontEnd/src/test/java/com/shopping/AddressRepositoryTests.java
@@ -87,4 +87,20 @@ public class AddressRepositoryTests {
         Address address = repo.findByIdAndCustomer(addressId, customerId);
         assertThat(address).isNull();
     }
+
+    @Test
+    public void testSetDefault() {
+        Integer addressId = 2;
+        repo.setDefaultAddress(addressId);
+
+        Address address = repo.findById(addressId).get();
+        assertThat(address.isDefaultForShipping()).isTrue();
+    }
+
+    @Test
+    public void testSetNonDefaultAddresses() {
+        Integer addressId = 2;
+        Integer customerId = 5;
+        repo.setNonDefaultForOthers(addressId, customerId);
+    }
 }
